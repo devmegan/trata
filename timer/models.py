077b793b419a791a36841d5b2e8c66cd5721d537
mvmolloy <mvamolloy@gmail.com>
2021-05-20 11:37:19 +0000

    add project/interval models

--- timer/models.py
+++ timer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return self.name
+
+class Interval(models.Model):
+    project = models.ForeignKey(Project, null=False, blank=False, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.description
