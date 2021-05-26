04dcc54ffb456434e9ff56b5abbfa8fac4e05b7c
mvmolloy <mvamolloy@gmail.com>
2021-05-26 11:01:42 +0000

    add index template with add project form

--- timer/views.py
+++ timer/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import Project
+from django.contrib.auth.models import User
 
 def index(request):
-    return HttpResponse("🍅 Tomato Time")
+
+    projects = None
+
+    if request.user.is_authenticated: 
+        projects = Project.objects.filter(user=request.user)
+
+    context = {
+        'projects': projects
+    }
+
+    return render(request, 'timer/index.html', context)
