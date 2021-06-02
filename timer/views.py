abc2b933838d121d5b2c0b11b5db5942e862efef
mvmolloy <mvamolloy@gmail.com>
2021-06-02 15:10:35 +0000

    simplify down project model

--- timer/views.py
+++ timer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Project, Interval
+from .models import Project
 from django.contrib.auth.models import User
 
 def index(request):
@@ -23,13 +23,10 @@ def project(request, project_id):
     """ view returns details specific project """
     
     project = get_object_or_404(Project, pk=project_id)
-    intervals = Interval.objects.filter(project=project_id)
-    print(intervals)
     context = {
         'project': project,
-        'intervals': intervals
+        'intervals_count': project.intervals
     }
-    
     return render(request, 'timer/project_detail.html', context)
 
 def add_project(request):
@@ -37,7 +34,7 @@ def add_project(request):
     """ view creates new project """ 
 
     if request.POST:
-        project = Project(user=request.user, name=request.POST['projectName'])
+        project = Project(user=request.user, name=request.POST['projectName'], intervals=0)
         project.save()
     
     return redirect('index')
